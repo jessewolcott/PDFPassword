444d302bef9a900a0abc7483e9a412ce917ae432
Jesse <jesse.wolcott@mac.com>
2023-03-30 14:52:10 -0400

    Update PasswordGenerator.ps1

--- PasswordGenerator.ps1
+++ PasswordGenerator.ps1
@@ -1,7 +1,7 @@
 ﻿$McDonalds = "http://nutrition.mcdonalds.com/nutrition1/nutritionfacts.pdf"
 $OliveGarden = "https://media.olivegarden.com/en_us/pdf/olive_garden_nutrition.pdf"
 $TacoBell = "https://mywisenutrition.files.wordpress.com/2012/08/taco-bell.pdf"
-$menu = $McDonalds
+$menu = $OliveGarden
 
 . .\CommonWords.ps1
 . .\BoringWords.ps1
@@ -20,7 +20,18 @@ $pattern = @"
 (\b\w{5,10})?\b
 "@
 
-$Phrases = ([regex]::Matches(($RawTextOutput -replace '[^a-zA-Z\s]','' -split ' ' ), $pattern).Value | Select-Object -Unique) | Where-Object {(($CommonWords+$BoringWords) -NotContains $_)}
+$PhraseBank = @([regex]::Matches(($RawTextOutput -replace '[^a-zA-Z\s]','' -split ' ' ), $pattern).Value | Select-Object -Unique) | Where-Object {(($CommonWords+$BoringWords) -NotContains $_)}
 
-$Phrases
-$Phrases.count
+## Password Generator ##
+#$PhraseBank = Get-Content .\PhraseBank.txt
+[String]$RandomNumber = (Get-Random -Maximum 99)
+$PW1       = $PhraseBank | Get-Random -ErrorAction SilentlyContinue
+$PW2       = $PhraseBank | Get-Random -ErrorAction SilentlyContinue
+$PW3       = $PhraseBank | Get-Random -ErrorAction SilentlyContinue
+$SpecialCharacters = (@((0,33,35) + (36..38) + (42..44) + (60..64) + (91..95)) | Get-Random -Count 1 | ForEach-Object {[char]$_})
+# https://thesysadminchannel.com/generate-strong-random-passwords-using-powershell/
+$PWGeneratedClearText = $PW1+$SpecialCharacters+$PW2+$RandomNumber+$PW3
+$PWGenerated = $PWGeneratedClearText | ConvertTo-SecureString -AsPlainText -Force
+## Password Generator ##
+
+$PWGeneratedClearText
