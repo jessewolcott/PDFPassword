5bcef2f86ac4fd6d8a4123166f9b4e376ce37516
Jesse <jesse.wolcott@mac.com>
2023-04-08 07:22:26 -0400

    PSM Creation

---
+++ Module/PDFPassword.psm1
@@ -0,0 +1,9 @@
+
+function Get-PDFPasswordDataSource {}
+function Set-PDFPasswordDataSource {}
+function Reset-PDFPasswordDataSource {}
+function Get-PDFPasswordConfiguration {}
+function Set-PDFPasswordConfiguration {}
+function New-PDFPassword {}
+function Get-PDFPasswordStrength {}
+
